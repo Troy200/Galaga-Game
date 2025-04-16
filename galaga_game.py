ac5b350342c0450fc614645cffedc0e28cb29a7d
Troy200 <kuriyanthomas27@gmail.com>
2025-04-16 16:55:35 +0200

    Add files via upload

--- galaga_game.py
+++ galaga_game.py
@@ -1,11 +1,12 @@
 import pgzrun
-import time
+
 
 HEIGHT= 700
 WIDTH = 1200
 
 bugs=[]
 
+bullets = []
 
 
 
@@ -27,7 +28,7 @@ for i in range(5):
     bugs.append(Bug)
 
 
-
+Points=0
 
 
 
@@ -40,6 +41,11 @@ def draw():
     for b in bugs:
         b.draw()
     Bullet.draw()
+    for b in bullets:
+        b.draw()
+    screen.draw.text(str(Points),(580,480))
+
+   
         
 
 
@@ -52,6 +58,25 @@ def update():
         Galaga.x=Galaga.x+15
         if Galaga.x>1180:
             Galaga.x=1180
+    if keyboard.space:
+        nbullet = Actor("bullet")
+        nbullet.x = Galaga.x
+        nbullet.y = Galaga.y
+        bullets.append(nbullet)
+
+    for bul in bullets():
+        bul.y -= 10
+        if bul.y < 0:
+            bullets.remove(bul)
+        else:
+            for bug in bugs():
+                if bul.colliderect(bug):
+                    bugs.remove(bug)
+                    bullets.remove(bul)
+                    Points += 10
+
+
+
     
 
 pgzrun.go()
